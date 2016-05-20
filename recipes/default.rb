ac79497234fde43bce1f64abd50054f5f3c040bb
Marcin Nowicki <pr0d1r2@gmail.com>
2016-05-20 17:59:37 +0200

    Support for chefdk kitchenplan config

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,43 +1,56 @@
-include_recipe "homebrewalt::default"
+if node['user'] && node['user']['id']
+  include_recipe "homebrew::default"
+  user_name = node['user']['id']
+  home_dir = Etc.getpwnam(user_name).dir
+  homebrew_tap "homebrew/versions"
+else
+  include_recipe "homebrewalt::default"
+  user_name = node['current_user']
+  home_dir = node['etc']['passwd'][user_name]['dir']
+  homebrewalt_tap "homebrew/versions"
+end
 
 ["homebrew.mxcl.elasticsearch17.plist" ].each do |plist|
-  plist_path = File.expand_path(plist, File.join('~', 'Library', 'LaunchAgents'))
+  plist_path = File.expand_path(plist, File.join(home_dir, 'Library', 'LaunchAgents'))
   if File.exists?(plist_path)
     log "elasticsearch17 plist found at #{plist_path}"
     execute "unload the plist (shuts down the daemon)" do
       command %'launchctl unload -w #{plist_path}'
-      user node['current_user']
+      user user_name
     end
   else
     log "Did not find plist at #{plist_path} don't try to unload it"
   end
 end
 
-[ "/Users/#{node['current_user']}/Library/LaunchAgents",
+[ "#{home_dir}/Library/LaunchAgents",
   PARENT_DATA_DIR,
   DATA_DIR ].each do |dir|
   directory dir do
-    owner node['current_user']
+    owner user_name
     action :create
   end
 end
 
-homebrewalt_tap "homebrew/versions"
 
 package "homebrew/versions/elasticsearch17" do
   action [:install, :upgrade]
 end
 
-execute 'brew pin homebrew/versions/elasticsearch17'
+execute 'brew pin homebrew/versions/elasticsearch17' do
+  user user_name
+end
 
-execute 'brew link elasticsearch17'
+execute 'brew link elasticsearch17' do
+  user user_name
+end
 
 execute "copy over the plist" do
   command %'cp /usr/local/Cellar/elasticsearch17/1.7.*/homebrew.mxcl.elasticsearch17.plist ~/Library/LaunchAgents/'
-  user node['current_user']
+  user user_name
 end
 
 execute "start the daemon" do
   command %'launchctl load -w ~/Library/LaunchAgents/homebrew.mxcl.elasticsearch17.plist'
-  user node['current_user']
+  user user_name
 end
